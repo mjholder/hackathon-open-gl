e23a7e18b0d5fb7d7bb9fc9fb30bf94344fa81e7
Matt Holder <mjholder96@gmail.com>
2022-05-23 14:00:13 -0400

    Initial app with example from GLFW

---
+++ build/CMakeFiles/hackathon-open-gl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hackathon-open-gl.dir/src/Application.cpp.o"
+  "CMakeFiles/hackathon-open-gl.dir/src/Application.cpp.o.d"
+  "hackathon-open-gl"
+  "hackathon-open-gl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hackathon-open-gl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/hackathon-open-gl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hackathon-open-gl.
+# This may be replaced when dependencies are built.
