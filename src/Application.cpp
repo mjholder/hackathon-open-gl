248c7632b1f543532e5752f27a21b6426afcb45d
Matt Holder <mjholder96@gmail.com>
2022-05-25 09:09:55 -0400

    sending data from program to shader via uniform

--- src/Application.cpp
+++ src/Application.cpp
@@ -138,6 +138,9 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // This is equivalent to V-Sync
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK){
         std::cout << "Error!" << std::endl;
     }
@@ -182,7 +185,16 @@ int main(void)
 
     ShaderProgramSource source = ParseShader("/home/maholder/git_repos/hackathon-open-gl/res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    // Binding shader to program
     GLCall(glUseProgram(shader));
+
+    // Retrieve location of u_Color variable
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    // Set value of u_Color in shader
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -190,8 +202,21 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
         /* Draw call that will draw our triangle */
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f)
+        {
+            increment = -0.05f;
+        }
+        if (r < 0.0f)
+        {
+            increment = 0.05f;
+        }
+
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
